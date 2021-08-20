444b737ffe95e9028bf490d4696fa1edfdb57ddd
crayjake <mm18j2c@leeds.ac.uk>
2021-08-20 12:34:50 +0100

    CDNs

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import * as TWEEN from '@tweenjs/tween.js';
+import * as THREE from 'https://cdn.skypack.dev/three';
+import * as TWEEN from 'https://cdn.skypack.dev/tween';
 
 
 //#region Constant Variables
